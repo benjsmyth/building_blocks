cb5fd0600c99eda82a6b51f98b547461d407f6b1
Ben Smyth <jbensmyth@gmail.com>
2018-06-28 18:48:11 -0400

    Complete stock picker

---
+++ stock_picker.rb
@@ -0,0 +1,10 @@
+def stock_picker(stocks)
+	pairs = {}
+	stocks.each do |base|
+		stocks.each do |value|
+			next if stocks.index(value) < stocks.index(base)
+			pairs[[stocks.index(base), stocks.index(value)]] = value - base
+		end
+	end
+	print pairs.key(pairs.values.max)
+end
