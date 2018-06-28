36b2a779708daa4c466e7308399d1bc503079e4a
Ben Smyth <jbensmyth@gmail.com>
2018-06-28 13:42:02 -0400

    Create files and complete Caesar

---
+++ caesar.rb
@@ -0,0 +1,11 @@
+def caesar(phrase, shift)
+	letters = phrase.split("")
+
+	letters.map! do |l|
+		if l =~ /[A-Z]/ then l = (65 + (((l.ord + (shift % 26)) % 65) % 26)).chr end
+		if l =~ /[a-z]/ then l = (97 + (((l.ord + (shift % 26)) % 97) % 26)).chr end
+		if l !=~ /[A-Za-z]/ then l end
+	end
+	
+	letters.join
+end 
